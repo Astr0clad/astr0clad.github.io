516711cbef9494742d2528aa1d0a645561ba12cd
astr0clad <colin.montigel@gmail.com>
2024-06-18 19:46:42 -0700

    Actually a decent site?
    Has animations + alts on images + is responsive

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
 var isToggle = true;
 
-function toggle() {
+async function toggle() {
     console.log("clicked");
     isToggle = !isToggle;
     if (isToggle) {
+        await gsap.to(".projects", { opacity: 0, duration: 0.5 });
+        document.getElementsByClassName('projects')[0].style.display = 'none';
         if (window.innerWidth <= 1000) {
             document.getElementsByClassName('renders')[0].style.display = 'flex';
         } else {
             document.getElementsByClassName('renders')[0].style.display = 'grid';
         }
-        document.getElementsByClassName('projects')[0].style.display = 'none';
+        await gsap.to(".renders", { opacity: 1, duration: 0.5 });
+    
     } else {
+        await gsap.to(".renders", { opacity: 0, duration: 0.5 });
         document.getElementsByClassName('renders')[0].style.display = 'none';
         document.getElementsByClassName('projects')[0].style.display = 'flex';
+        await gsap.to(".projects", { opacity: 1, duration: 0.5 });
     }
 }
 
